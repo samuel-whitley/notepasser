4a25c1c32b0d43b251d2dd7610c789a65bfabf41
samuel <samuel.whitley89@gmail.com>
2015-01-30 12:43:37 -0500

    can add users and stuff need to fix my regexs so I can get rid of some classes

--- lib/notepasser.rb
+++ lib/notepasser.rb
@@ -9,7 +9,44 @@ module Notepasser
 end
 
 module Notepasser::Models
+  class User < ActiveRecord::Base
+    has_many :messages, dependent: :destroy
+  end
+
+  class Message < ActiveRecord::Base
+    belongs_to :user
+  end
 end
 
 module Notepasser::Controllers
+    class Users < R '/user/([^/]+)'
+      def get(name)
+      user = Notepasser::Models::User.where(:name == name)
+      user.to_json
+      
+      end
+    end
+
+    class Make < R '/user/make/([^/]+)'
+      def post(name)
+        new_usr = Notepasser::Models::User.find_or_create_by(:name => name)
+       new_usr.save
+       new_usr.to_json
+     end
+    end
+
+    class List < R '/user/list/'
+      def get
+        dis = Notepasser::Models::User.group(:name)
+        dis.to_json
+      end
+    end
+    
+    class Send
+      def patch(msg = {})
+
+       #send = Notepasser::Models::Message.create(:user_id => ??? ,:content => , :read => false)
+      end
+    end
 end
+binding.pry
